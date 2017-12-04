d7021d5df00847b1b8096257849ca36486118f18
AsoTiesto <cute17852337569@gmail.com>
2017-12-04 23:06:47 +0800

    test

--- src/js/event-countingAwards.js
+++ src/js/event-countingAwards.js
@@ -9,26 +9,11 @@ var getAwards = function() {
     function(jsonData) {
       console.log("成功抓取使用者獎勵累積！");
       var data = jsonData.content;
-      console.log(data);
-
-      data.forEach(function(val, idx) {
-        console.log(val);
-        console.log(idx);
+      console.log("data: " + data);
+      data.forEach(element => {
+        console.log(element);
       });
     },
     function() {}
   );
 };
-
-// var getConditionAwards = function(awardId) {
-//   ajaxGet(
-//     "https://test.ehanlin.com.tw/chest/condition/one/" + awardId,
-//     null,
-//     function(jsonData) {
-//       console.log("成功抓取awardId資料！");
-//       var awardName = jsonData.content.desc;
-//       console.log("awardName: " + awardName);
-//     },
-//     function() {}
-//   );
-// };
