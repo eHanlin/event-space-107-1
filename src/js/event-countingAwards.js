b3b6ce386169ff00643428afc8390e47de7e160c
AsoTiesto <cute17852337569@gmail.com>
2017-12-04 23:23:21 +0800

    test

--- src/js/event-countingAwards.js
+++ src/js/event-countingAwards.js
@@ -8,7 +8,13 @@ var getAwards = function() {
     null,
     function(jsonData) {
       console.log("成功抓取使用者獎勵累積！");
-      console.log("data: " + jsonData);
+      var data = JSON.stringify(jsonData);
+      console.log("data: " + data);
+
+      for (var property in jsonData) {
+        output += property + ": " + jsonData[property] + "; ";
+      }
+      console.log("output:" + output);
     },
     function() {}
   );
