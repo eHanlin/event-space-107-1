7385dc65064eff60a8dc9b289cee540ab7ece5f8
AsoTiesto <cute17852337569@gmail.com>
2017-12-04 19:14:56 +0800

    test

--- dist/js/event-countingAwards.js
+++ dist/js/event-countingAwards.js
@@ -10,24 +10,30 @@ var getAwards = function() {
       console.log("成功抓取使用者獎勵累積！");
       var data = jsonData.content;
       console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        $(".countingAwards").append(data[i]);
-        getConditionAwards(data[i]);
-      }
+
+      data.forEach(function(value, key) {
+        if (value % 2 === 1) {
+          $(".countingAwards .odd .key").append(key);
+          $(".countingAwards .odd .value").append(value);
+        } else {
+          $(".countingAwards .even .key").append(key);
+          $(".countingAwards .even .value").append(value);
+        }
+      });
     },
     function() {}
   );
 };
 
-var getConditionAwards = function(awardId) {
-  ajaxGet(
-    "https://test.ehanlin.com.tw/chest/condition/one/" + awardId,
-    null,
-    function(jsonData) {
-      console.log("成功抓取awardId資料！");
-      var awardName = jsonData.content.desc;
-      console.log("awardName: " + awardName);
-    },
-    function() {}
-  );
-};
+// var getConditionAwards = function(awardId) {
+//   ajaxGet(
+//     "https://test.ehanlin.com.tw/chest/condition/one/" + awardId,
+//     null,
+//     function(jsonData) {
+//       console.log("成功抓取awardId資料！");
+//       var awardName = jsonData.content.desc;
+//       console.log("awardName: " + awardName);
+//     },
+//     function() {}
+//   );
+// };
