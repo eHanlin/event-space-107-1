0830a98c45855bb6378c1cf77ddca43e41d24a02
infinityAlive <sonic@ehanlin.com.tw>
2017-12-06 08:12:37 +0800

    fix bugs

--- dist/js/event-message.js
+++ dist/js/event-message.js
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-$("#down-arrow").on("click",function(){$(this).hide(),$(".message .fadeMessage").removeAttr("style"),$(".background").attr("style","height:5900px"),$("#up-arrow").show()}),$("#up-arrow").on("click",function(){$(this).hide(),$(".message .fadeMessage").hide(),$(".background").attr("style","height:2950px"),$("#down-arrow").show()});
-=======
-$("#down-arrow").on("click", function() {
-  $(this).hide();
-  $(".message .fadeMessage").removeAttr("style");
-  $(".background").attr("style", "height:4600px");
-  $("#up-arrow").show();
-});
-
-$("#up-arrow").on("click", function() {
-  $(this).hide();
-  $(".message .fadeMessage").hide();
-  $(".background").attr("style", "height:2650px");
-  $("#down-arrow").show();
-});
->>>>>>> 92e43870c198e70026ba332c4f3fbcb2ae3beed6
+$("#down-arrow").on("click",function(){$(this).hide(),$(".message .fadeMessage").removeAttr("style"),$(".background").attr("style","height:4600px"),$("#up-arrow").show()}),$("#up-arrow").on("click",function(){$(this).hide(),$(".message .fadeMessage").hide(),$(".background").attr("style","height:2650px"),$("#down-arrow").show()});
