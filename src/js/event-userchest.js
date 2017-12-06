291e0743819e85989f2eb06747610319b8d437d5
AsoTiesto <cute17852337569@gmail.com>
2017-12-06 11:56:31 +0800

    test

--- src/js/event-userchest.js
+++ src/js/event-userchest.js
@@ -62,11 +62,6 @@ var determineStatus = function(chest, thisPlatformTarget, chestSatuts) {
     thisPlatformTarget.find(".upgradeButton").hide();
   }
 
-  if (chestId === null) {
-    thisPlatformTarget.find(".startButton").hide();
-    thisPlatformTarget.find(".upgradeButton").hide();
-  }
-
   if (chestSatuts === "LOCKED") {
     console.log("===============> status is locked <=================");
     determineLevel(chestTarget, chestLevel);
