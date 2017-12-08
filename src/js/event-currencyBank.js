3e152e3dd57ea2539b0ea98e55994aa8b0a70f46
AsoTiesto <cute17852337569@gmail.com>
2017-12-08 12:24:22 +0800

    test

--- src/js/event-currencyBank.js
+++ src/js/event-currencyBank.js
@@ -7,39 +7,51 @@ $(function() {
       var singleTransaction, transactionField;
       var currencyQuantity;
       var tbodyHtml, tds;
+      var longDateFormat = "dd/MM/yyyy HH:mm:ss";
 
       for (var i = 0; i < transactions.length; i++) {
         singleTransaction = transactions[i];
         currencyQuantity = singleTransaction["currencyQuantity"];
 
+        // 動作
         if (singleTransaction.action == "REDUCE") {
           tds = "<td>" + "消耗" + "</td>";
-        } else if (singleTransaction.action.match("ADD")) {
-          tds = "<td>" + "獲得" + "</td>";
+        } else if (singleTransaction.action == "ADD") {
+          tds = "<td>" + "增加" + "</td>";
         } else if (singleTransaction.action == "ADD_BY_CHEST") {
-          tds = "<td>" + "獲得(寶箱)" + "</td>";
+          tds = "<td>" + "增加" + "</td>";
         } else {
           tds = "<td>" + singleTransaction["action"] + "</td>";
         }
 
+        // e幣
         tds +=
           "<td>" +
           (currencyQuantity["coins"] ? currencyQuantity["coins"] : 0) +
           "</td>";
+
+        // 寶石
         tds +=
           "<td>" +
           (currencyQuantity["gems"] ? currencyQuantity["gems"] : 0) +
           "</td>";
 
+        // 來源
         tds += "<td>" + singleTransaction["detail"] + "</td>";
 
+        // 事件
         if (singleTransaction.source.match("Chest")) {
           tds += "<td>" + "寶箱" + "</td>";
         } else {
           tds += "<td>" + singleTransaction["source"] + "</td>";
         }
 
-        tds += "<td>" + singleTransaction["updateTime"] + "</td>";
+        // 時間
+        tds +=
+          "<td>" +
+          singleTransaction["updateTime"].format(longDateFormat) +
+          "</td>";
+
         tbodyHtml += "<tr>" + tds + "</tr>";
       }
       $(".responseTable tbody").append(tbodyHtml);
