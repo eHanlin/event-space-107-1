4f257ff913959ac2b2451e8b81e9857c4dcbe9f5
AsoTiesto <cute17852337569@gmail.com>
2017-12-15 11:19:15 +0800

    no message

--- src/js/event-countdown.js
+++ src/js/event-countdown.js
@@ -32,7 +32,7 @@ let updateStatusIsUnlocking = function(chestId, startBtnTarget) {
 
 let coolDownTime = function(chestId) {
   let countDown = function(jsonData, chestId) {
-    let seconds = jsonData.content;
+    let seconds;
     let remainHours = Math.ceil(seconds / 3600);
     let platformTarget = $("#" + chestId);
     let imgChestTarget = platformTarget.find(".chest");
@@ -42,10 +42,7 @@ let coolDownTime = function(chestId) {
     imgChestTarget.addClass("unlockingGray");
     openNowBtnTarget.removeAttr("style");
 
-    // 立即開啟按鈕
-    openNowBtnFunc(remainHours);
-
-    let countDownFunc = function() {
+    let countDownFunc = function(seconds) {
       countdownTarget.countDown({
         timeInSecond: seconds,
         displayTpl:
@@ -59,7 +56,75 @@ let coolDownTime = function(chestId) {
         }
       });
     };
-    countDownFunc();
+
+    var getConditionOpenImmediately = function(remainHours, chestId) {
+      ajaxGet(
+        "https://test.ehanlin.com.tw/chest/condition/one/openImmediately",
+        null,
+        function(jsonData) {
+          let data = jsonData.content.content;
+          let everyHourDeductGems = data.everyHourDeductGems;
+          let deductGems = remainHours * everyHourDeductGems;
+          let openNowBtnTarget = $("#" + chestId).find(".openNowButton");
+          let imgChestTarget = $("#" + chestId).find(".chest");
+          console.log("deductGems:" + deductGems);
+
+          $.confirm(
+            confirmWindow(
+              "立即開啟寶箱需要花費 " + deductGems + " 個寶石",
+              "確定立即開啟寶箱嗎？",
+              function() {
+                getOpenChestImmediately(chestId);
+
+                // openNowBtnTarget.fadeOut();
+                // eventChest.updateStatusIsReady(chestId);
+                // imgChestTarget.removeClass("unlockingGray");
+
+                countDownFunc(0);
+              }
+            )
+          );
+        },
+        function() {}
+      );
+    };
+
+    // 立即開啟按鈕
+    let openNowBtnFunc = function(remainHours) {
+      $(".container .space .openNowButton[data-onlocked=false]").on(
+        "click",
+        function() {
+          let chestId;
+          $(this).attr("data-onlocked", "true");
+
+          chestId = $(this)
+            .parents(".platform")
+            .prop("id");
+
+          getConditionOpenImmediately(remainHours, chestId);
+        }
+      );
+    };
+
+    let getOpenChestImmediately = function(chestId) {
+      ajax(
+        "PUT",
+        "https://test.ehanlin.com.tw/chest/open/immediately/" + chestId,
+        {
+          status: "READY"
+        },
+        function(jsonData) {
+          console.log("成功抓取 openImmediately !!!");
+          console.log(jsonData.content);
+        }
+      );
+    };
+
+    // 立即開啟按鈕
+    openNowBtnFunc(remainHours);
+
+    seconds = jsonData.content;
+    countDownFunc(seconds);
   };
 
   ajaxGet(
