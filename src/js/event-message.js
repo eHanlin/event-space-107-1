330e187e804fdc7b99d1f1e8d8afe9af76a64b29
AsoTiesto <cute17852337569@gmail.com>
2017-12-20 12:32:12 +0800

    更新活動辦法文字

--- src/js/event-message.js
+++ src/js/event-message.js
@@ -1,13 +1,13 @@
 $("#down-button").on("click", function() {
   $(this).hide();
   $(".message .fadeMessage").removeAttr("style");
-  $(".background").attr("style", "height:5470px");
+  $(".background").attr("style", "height:5500px");
   $("#up-button").show();
 });
 
 $("#up-button").on("click", function() {
   $(this).hide();
   $(".message .fadeMessage").hide();
-  $(".background").attr("style", "height:2940px");
+  $(".background").attr("style", "height:2980px");
   $("#down-button").show();
 });
