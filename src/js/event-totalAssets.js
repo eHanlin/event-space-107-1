4896dc4e4be62e534f14adea295bbff7f93a9445
infinityAlive <sonic@ehanlin.com.tw>
2017-12-28 17:08:52 +0800

    [#3140](https://github.com/eHanlin/tutor/issues/3140) 彈跳視窗改為登入後觸發

--- src/js/event-totalAssets.js
+++ src/js/event-totalAssets.js
@@ -1,15 +1,41 @@
-$(function() {
+$(function () {
   ajaxGet(
     "/currencyBank/totalAssets/retrieve/one",
     null,
-    function(jsonData) {
+    function (jsonData) {
       $(".bank").removeAttr("style");
       $(".space .coins span").append(jsonData.content["coins"]);
       $(".space .gems span").append(jsonData.content["gems"]);
     },
 
-    function() {
+    function () {
+      let popup = function () {
+        if ( !Cookies.get("showSpaceTreasure") ) {
+          let endOfDay, todayExpiredMilliseconds;
+          $(".space-treasure-popup").show();
+
+          moment.locale();
+          endOfDay = moment().startOf("day").endOf("day");
+          todayExpiredMilliseconds = endOfDay.diff(moment());
+          console.log(todayExpiredMilliseconds);
+
+          Cookies.set("showSpaceTreasure", "true", {
+            expires: new Date(new Date().getTime() + todayExpiredMilliseconds),
+            path: ""
+          });
+
+          setTimeout(function () {
+            $(".space-treasure").addClass("space-treasure-show");
+            $(".space-treasure .space-treasure-understand").on("click", function () {
+              $(".space-treasure-popup").remove();
+            });
+          }, 500);
+        }
+      };
+
       $(".bankLogout").removeAttr("style");
+      popup();
+
     }
   );
 });
